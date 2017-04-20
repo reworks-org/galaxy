85da0b87ae9010931d4b60adec858b8c7814df22
Dom-mac <dev.dom.re@gmail.com>
2017-04-20 14:06:19 +0800

    framework for debug manager

---
+++ REngine3/source/re/debug/DebugManager.cpp
@@ -0,0 +1,14 @@
+//
+//  DebugManager.cpp
+//  REngine3
+//
+//  Created by reworks on 20/04/2017.
+//  Copyright (c) 2017 reworks. All rights reserved.
+//
+
+#include "DebugManager.hpp"
+
+namespace re
+{
+    
+}
---
+++ REngine3/source/re/debug/DebugManager.hpp
@@ -0,0 +1,24 @@
+//
+//  DebugManager.hpp
+//  REngine3
+//
+//  Created by reworks on 20/04/2017.
+//  Copyright (c) 2017 reworks. All rights reserved.
+//
+
+#ifndef RENGINE3_DEBUGMANAGER_HPP_
+#define RENGINE3_DEBUGMANAGER_HPP_
+
+#include "re/types/Service.hpp"
+#include "re/services/ServiceLocator.hpp"
+
+namespace re
+{
+    class DebugManager : public Service
+    {
+    public:
+        
+    };
+}
+
+#endif
