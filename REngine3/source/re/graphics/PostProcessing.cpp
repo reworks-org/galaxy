50a3c0d716714c3d81f340bd00bd682e2900afdb
Dom-mac <dev.dom.re@gmail.com>
2017-05-29 12:25:22 +0800

    began on post effects

---
+++ REngine3/source/re/graphics/PostProcessing.cpp
@@ -0,0 +1,14 @@
+//
+//  PostProcessing.cpp
+//  REngine3
+//
+//  Created by reworks on 29/5/17.
+//  Copyright (c) 2017 reworks. All rights reserved.
+//
+
+#include "PostProcessing.hpp"
+
+namespace re
+{
+    
+}
