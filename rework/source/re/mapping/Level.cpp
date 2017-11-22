82cef9ff27f6c6f40a658cfd22d10e196ca993f3
reworks-desktop <dev.dom.re@gmail.com>
2017-11-22 18:23:50 +0800

    work on levels

--- rework/source/re/mapping/Level.cpp
+++ rework/source/re/mapping/Level.cpp
@@ -6,16 +6,34 @@
 //  Copyright (c) 2017 reworks. All rights reserved.
 //
 
+#include "sol2/sol.hpp"
+#include "re/services/VFS.hpp"
+#include "re/services/ServiceLocator.hpp"
+
 #include "Level.hpp"
 
 namespace re
 {
-	Level::Level(const std::string& map)
-	:m_map(map)
+	Level::Level(const std::string& script)
 	{
+		sol::state lua;
+		lua.script(Locator::get<VFS>()->openAsString(script));
+		sol::table level = lua.get<sol::table>("level");
+
+		m_map = tmx_load(level.get<std::string>("map");
+		m_dimensions.x = level.get<float>("x");
+		m_dimensions.y = level.get<float>("y");
+		m_dimensions.width = level.get<int>("width");
+		m_dimensions.height = level.get<int>("height");
 	}
 
 	Level::~Level()
 	{
+		tmx_map_free(m_map);
+	}
+
+	Rect<int>& Level::getDimensions() const
+	{
+		return m_dimensions;
 	}
 }
--- rework/source/re/mapping/Level.hpp
+++ rework/source/re/mapping/Level.hpp
@@ -9,19 +9,42 @@
 #ifndef REWORK_LEVEL_HPP_
 #define REWORK_LEVEL_HPP_
 
-#include "re/mapping/TMXMap.hpp"
+#include <vector>
+
+#include "re/math/Rect.hpp"
+#include "re/mapping/TMXHelper.hpp"
+
+// grab tiles between camera coords
+// in order to only render those
+// since we need to refresh the map every time we update the quadtree
+// we can then update the animations aswell. no need to worry about loading
+// static map once.
+// unless we only render a subsection of an al_bitmap.
+// might be a better idea?
+// we shall see.
+// each layer should be a bitmap so we can instert it into layer render engine
+// movesystem needs to grab tile information in order to make actions occur
+// new idea: break up levels into seperate components to be processed by a level system! eggcelent idea! :D
 
 namespace re
 {
 	class Level
 	{
 	public:
-		Level(const std::string& map);
+		Level(const std::string& script);
 		~Level();
 
+		///
+		/// Return current map dimensions.
+		///
+		/// \return Rect<int> CONST
+		///
+		Rect<int>& getDimensions() const;
+
 	private:
-		TMXMap m_map;
+		tmx_map* m_map;
 		Rect<float, int> m_dimensions;
+		std::vector<ALLEGRO_BITMAP*> m_layers;
 	};
 }
 
