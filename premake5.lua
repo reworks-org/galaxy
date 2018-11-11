674faddabe01a82ed648ea4cb5ad51f3064a713f
reworks-desktop <dev.dom.re@gmail.com>
2018-11-11 17:07:17 +0800

    adjustments

--- premake5.lua
+++ premake5.lua
@@ -12,33 +12,23 @@ workspace "Quasar"
 
 
 ---
---- START PLATFORM FILTERS
+--- START ARCHITECTURE FILTERS
 ---
 	filter "platforms:Win32"
-		system "windows"
 		architecture "x32"
 		defines { "WIN32" }
-		includedirs { "Quasar/src", "libs/SDL2/include" }
 		libdirs { "libs/SDL2/lib/x32" }
 
 	filter "platforms:Win64"
-		system "windows"
 		architecture "x64"
 		defines { "WIN64" }
-		includedirs { "Quasar/src", "libs/SDL2/include" }
 		libdirs { "libs/SDL2/lib/x64" }
 
 	filter "platforms:Linux32"
-		system "linux"
 		architecture "x32"
-		includedirs { "Quasar/src", os.findheader("libsdl2-dev") }
-		libdirs { os.findlib("libsdl2-2.0.0") }
 
 	filter "platforms:Linux64"
-		system "linux"
 		architecture "x64"
-		includedirs { "Quasar/src", os.findheader("libsdl2-dev") }
-		libdirs { os.findlib("libsdl2-2.0.0") }
 ---
 --- END PLATFORM FILTERS
 ---
@@ -62,17 +52,20 @@ workspace "Quasar"
 
 
 ---
---- START SPECIFIC FILTERS
+--- START PLATFORM FILTERS
 ---
 	filter { "platforms:Win32 or Win64" }
+		system "windows"
+		includedirs { "Quasar/src", "libs/SDL2/include" }
 		flags { "NoPCH" }
 		defines { "_SILENCE_ALL_CXX17_DEPRECATION_WARNINGS", "_CRT_SECURE_NO_WARNINGS", "_CRT_SECURE_NO_DEPRECATE", "_CRT_NONSTDC_NO_DEPRECATE" }
 		buildoptions { "/bigobj" }
-
-	filter { "platforms:Win32 or Win64"}
 		links { "OpenGL32" }
 
 	filter { "platforms:Linux32 or Linux64"}
+		system "linux"
+		includedirs { "Quasar/src", os.findheader("libsdl2-dev") }
+		libdirs { os.findlib("libsdl2-2.0.0") }
 		links { "GL", "dl", "pthread" }
 ---
 --- END SPECIFIC FILTERS
