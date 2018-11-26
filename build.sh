755030d55266636aff1b20909db7d23a0b573f72
reworks-desktop <dev.dom.re@gmail.com>
2018-11-26 11:49:36 +0800

    sigh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update
 sudo apt-get install -y curl build-essential g++-8 libxml2 libxml2-dev liballegro5.2 liballegro5-dev
 curl -L -o premake5.tar.gz https://github.com/premake/premake-core/releases/download/v5.0.0-alpha13/premake-5.0.0-alpha13-linux.tar.gz
