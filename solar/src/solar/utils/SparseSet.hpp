87927c80f14cd8296b8d9a78a10aebde9f7a9703
domre-desktop <dev.dom.re@gmail.com>
2019-04-25 13:37:33 +0800

    further implementation

--- solar/src/solar/utils/SparseSet.hpp
+++ solar/src/solar/utils/SparseSet.hpp
@@ -18,14 +18,19 @@ namespace sr
 	{
 	public:
 		SparseSet();
+		SparseSet(std::size_t reserve);
 
 		void insert(Type x);
+		
 		bool has(Type x);
 
-		void reserve();
-		bool empty();
-		std::size_t size();
-		std::size_t capacity();
+		void reserve(std::size_t reserve);
+
+		bool empty() noexcept;
+
+		std::size_t size() const noexcept;
+
+		std::size_t capacity() const noexcept;
 
 	private:
 		std::size_t m_size;
@@ -37,8 +42,18 @@ namespace sr
 
 	template<typename Type>
 	inline SparseSet<Type>::SparseSet()
+		:m_size(0), m_capacity(0)
 	{
 		static_assert(std::is_unsigned<T>::value, "SparseSet can only contain unsigned integers");
+	}
+
+	template<typename Type>
+	inline SparseSet<Type>::SparseSet(std::size_t reserve)
+		:m_size(0), m_capacity(0)
+	{
+		static_assert(std::is_unsigned<T>::value, "SparseSet can only contain unsigned integers");
+
+		reserve(reserve);
 	}
 
 	template<typename Type>
@@ -60,6 +75,35 @@ namespace sr
 		}
 
 		return success;
+	}
+
+	template<typename Type>
+	inline void SparseSet<Type>::reserve(std::size_t reserve)
+	{
+		if (reserve > m_capacity)
+		{
+			m_denseSet.reserve(reserve);
+			m_sparseSet.reserve(reserve);
+			m_capacity = reserve;
+		}
+	}
+
+	template<typename Type>
+	inline bool SparseSet<Type>::empty() noexcept
+	{
+		return m_size == 0;
+	}
+
+	template<typename Type>
+	inline std::size_t SparseSet<Type>::size() const noexcept
+	{
+		return m_size;
+	}
+
+	template<typename Type>
+	inline std::size_t SparseSet<Type>::capacity() const noexcept
+	{
+		return m_capacity;
 	}
 }
 
