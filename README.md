c52ee75f67bdc0ebd382233dd250c86bed0bc3ad
domre-desktop <dev.dom.re@gmail.com>
2019-11-14 14:51:46 +0800

    docs update

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # solar
-Basic ECS, written in C++17.
-Based off of:
+Entity-Component-System, written in C++17.
 
+## Tutorial
+See [/testbed/src/test/](https://github.com/DomRe/solar/tree/master/testbed/src/test).
+
+## Credits
+https://skypjack.github.io/2019-02-14-ecs-baf-part-1/
 https://skypjack.github.io/2019-03-21-ecs-baf-part-2-insights/
-
-and
-
+https://skypjack.github.io/2019-09-25-ecs-baf-part-5/
 https://www.computist.xyz/2018/06/sparse-sets.html
+https://www.geeksforgeeks.org/sparse-set/
--- testbed/src/test/main.cpp
+++ testbed/src/test/main.cpp
@@ -8,58 +8,11 @@
 #include "gtest/gtest.h"
 #include <solar/entity/Manager.hpp>
 
-struct a { a(int newvar) { var = newvar; } int var = 100; };
-struct b { int var = 200; };
-
-struct TestA
-{
-	unsigned int val = 100;
-};
-
-struct TestB
-{
-	TestB(unsigned int v)
-	{
-		val = v;
-	}
-
-	unsigned int val = 200;
-};
-
 int main(int argc, char **argv)
 {
-	//sr::Manager hs;
-
-	//sr::Entity ea = hs.create();
-	//sr::Entity eb = hs.create();
-	//sr::Entity ec = hs.create();
-	//hs.add<a>(ea, 50);
-	//hs.add<b>(ea);
-
-	//std::cout << "ea: " << ea << "\neb: " << eb << "\nec: " << ec << std::endl;
-	{
-		/* structured binding... */
-		//auto [x, y] = hs.multi<a, b>(ea);
-
-		//std::cout << sr::cuid::uid<decltype(std::get<0>(t))>() << std::endl;
-		//std::cout << sr::cuid::uid<decltype(std::get<1>(t))>() << std::endl;
-
-		//std::cout << std::get<0>(t)->var << std::endl;
-		//std::cout << std::get<1>(t)->var << std::endl;
-
-		/*
-		hs.operate<a, b>([](sr::Entity entity, a* ca, b* cb)
-		{
-			cb->var = 500;
-		});*/
-
-		//std::cout << std::get<1>(t)->var << std::endl;
-	}
-
 	testing::InitGoogleTest(&argc, argv);
-
 	auto result = RUN_ALL_TESTS();
-
+	
 	std::cin.get();
 
 	return result;
