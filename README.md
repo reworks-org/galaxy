818fd05dddbb07fc3abe7921f4d64b1f4ec34ba5
domre-desktop <dev.dom.re@gmail.com>
2019-12-24 15:35:57 +0800

    project framework

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # redshift
-C++ 2D physics engine.
+[![Build Status]()
+[![Documentation Status](https://travis-ci.org/DomRe/redshift.svg?branch=master)](https://domre.github.io/redshift/)
+
+Modern C++ 2D physics engine.
+
+## Usage:
+```cpp
+// todo
+```  
+
+### Documentation:  
+See [documentation](https://domre.github.io/redshift/) for more.  
+
+### Credits
+* [doxygen-dark-theme](https://github.com/MaJerle/doxygen-dark-theme).
---
+++ testbed/src/testbed.cpp
@@ -0,0 +1,32 @@
+///
+/// testbed.cpp
+/// starlight
+///
+/// Refer to LICENSE.txt for more details.
+///
+
+#include <iostream>
+
+#include "starlight/event/Base.hpp"
+#include "starlight/event/Wrapper.hpp"
+#include "starlight/Dispatcher.hpp"
+
+struct Test
+{
+	int a = 10;
+};
+
+int main()
+{
+	Test test;
+	starlight::Dispatcher dispatcher;
+
+	dispatcher.add<Test>([](const Test& test)
+	{
+		std::cout << test.a << std::endl;
+	});
+
+	dispatcher.trigger<Test>(test);
+
+	std::cin.get();
+}
