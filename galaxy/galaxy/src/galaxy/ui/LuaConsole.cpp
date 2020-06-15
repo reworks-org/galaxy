79942c9e75a294b26b702a437ad744a4712937f8
domre-desktop <dev.dom.re@gmail.com>
2020-06-15 22:15:28 +0800

    improvements to lua console

--- galaxy/galaxy/src/galaxy/ui/LuaConsole.cpp
+++ galaxy/galaxy/src/galaxy/ui/LuaConsole.cpp
@@ -25,7 +25,7 @@ namespace galaxy
 	void LuaConsole::draw(bool* show) noexcept
 	{
 		ImGui::Begin("Lua Console", show, ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_AlwaysVerticalScrollbar);
-		ImGui::SetWindowSize(ImVec2(520, 600), ImGuiCond_FirstUseEver);
+		ImGui::SetWindowSize(ImVec2(800, 600), ImGuiCond_FirstUseEver);
 
 		if (ImGui::BeginMenuBar())
 		{
@@ -41,11 +41,13 @@ namespace galaxy
 			}
 
 			ImGui::SameLine();
+			ImGui::PushStyleColor(ImGuiCol_FrameBg, ImVec4(1.0f, 1.0f, 1.0f, 1.0f));
+			ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.0f, 0.0f, 0.0f, 1.0f));
 
 			static std::string s_buff = "";
 			if (ImGui::InputText("", &s_buff, ImGuiInputTextFlags_EnterReturnsTrue | ImGuiInputTextFlags_AutoSelectAll | ImGuiInputTextFlags_NoUndoRedo))
 			{
-				m_history.push_back(s_buff);
+				m_history.push_back("INPUT: " + s_buff);
 				std::string code = "";
 
 				if (std::filesystem::path(s_buff).extension() == ".lua")
@@ -59,37 +61,14 @@ namespace galaxy
 					code = s_buff;
 				}
 
-				auto res = SL_HANDLE.lua()->safe_script(code);
-				if (res.get_type() == sol::type::boolean)
-				{
-					bool val = res.get<bool>();
-					if (val)
-					{
-						m_history.push_back("true");
-					}
-					else
-					{
-						m_history.push_back("false");
-					}
-				}
-				else if (res.get_type() == sol::type::number)
-				{
-					double val = res.get<double>();
-					m_history.push_back(std::to_string(val));
-				}
-				else if (res.get_type() == sol::type::string)
-				{
-					m_history.push_back(res.get<std::string>());
-				}
-				else
-				{
-					m_history.push_back("Success!");
-				}
+				std::string res = SL_HANDLE.lua()->safe_script(code);
+				m_history.push_back("OUTPUT: " + res);
 
 				s_buff.clear();
 				ImGui::SetKeyboardFocusHere(-1);
 			}
 
+			ImGui::PopStyleColor(2);
 			ImGui::EndMenuBar();
 		}
 
