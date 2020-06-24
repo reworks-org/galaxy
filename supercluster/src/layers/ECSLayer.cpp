9f28bad5d6e1954d6d95a4e6f368c44e6bd596f9
domre-desktop <dev.dom.re@gmail.com>
2020-06-24 19:49:45 +0800

    demo test

--- supercluster/src/layers/ECSLayer.cpp
+++ supercluster/src/layers/ECSLayer.cpp
@@ -7,6 +7,11 @@
 
 #include <galaxy/core/ServiceLocator.hpp>
 #include <galaxy/systems/RenderSystem.hpp>
+#include <galaxy/components/SpriteComponent.hpp>
+#include <galaxy/components/ShaderComponent.hpp>
+#include <galaxy/components/TransformComponent.hpp>
+#include <galaxy/flags/EnabledFlag.hpp>
+#include <qs/shaders/Sprites.hpp>
 
 #include "ECSLayer.hpp"
 
@@ -22,6 +27,21 @@ namespace sc
 		m_world = SL_HANDLE.world();
 
 		m_camera.create(0.0f, m_window->getWidth(), m_window->getHeight(), 0.0f);
+
+		/*
+		auto entity = m_world->create();
+		m_world->add<galaxy::EnabledFlag>(entity);
+
+		auto spr = m_world->add<galaxy::SpriteComponent>(entity);
+		spr->load("wall.png");
+		spr->create<qs::BufferTypeDynamic>();
+
+		auto tf = m_world->add<galaxy::TransformComponent>(entity);
+		tf->m_transform.move(10.0f, 10.0f);
+
+		auto sh = m_world->add<galaxy::ShaderComponent>(entity);
+		sh->m_shader.loadFromRaw(qs::s_spriteVS, qs::s_spriteFS);
+		*/
 	}
 
 	ECSLayer::~ECSLayer() noexcept
