ae544034660a0c367eb574a67db68cac26529ba2
domre-desktop <dev.dom.re@gmail.com>
2020-06-25 14:35:21 +0800

    setup googletest

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -5,6 +5,17 @@
 /// Refer to LICENSE.txt for more details.
 ///
 
+#include <gtest/gtest.h>
+#include <pulsar/Log.hpp>
+
 int main(int argsc, char* argsv[])
 {
+	PL_ENABLE_TESTING_MODE;
+
+	testing::InitGoogleTest(&argsc, argsv);
+	auto result = RUN_ALL_TESTS();
+
+	std::cin.get();
+
+	return result;
 }
