00926faf99ddffcb432a316b74e1f3959cf5d85a
domre-desktop <dev.dom.re@gmail.com>
2020-06-26 19:06:48 +0800

    Update coverage.bat

--- coverage.bat
+++ coverage.bat
@@ -1,3 +1,3 @@
 if "%CONFIGURATION%"=="Debug" (
-	OpenCppCoverage --sources c:\projects\galaxy --modules c:\projects\galaxy --export_type=cobertura:cobertura.xml -- C:\projects\galaxy\tests\bin\%CONFIGURATION%\%PLATFORM%\tests.exe & codecov --root .. --no-color --disable gcov -f cobertura.xml -t %CODECOV_TOKEN%
+	OpenCppCoverage --sources c:\projects\galaxy --modules c:\projects\galaxy --export_type=cobertura:cobertura.xml -- C:\projects\galaxy\tests\bin\%CONFIGURATION%\%PLATFORM%\tests.exe & codecov --root .. -f cobertura.xml -t %CODECOV_TOKEN%
 )
