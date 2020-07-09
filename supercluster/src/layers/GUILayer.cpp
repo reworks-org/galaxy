adc9c4c9d615adf3dce6c9c02d0f515b6c4bea06
domre-desktop <dev.dom.re@gmail.com>
2020-07-09 13:21:34 +0800

    implemented test gui to test widgets

--- supercluster/src/layers/GUILayer.cpp
+++ supercluster/src/layers/GUILayer.cpp
@@ -9,6 +9,7 @@
 #include <galaxy/res/FontBook.hpp>
 #include <galaxy/fs/FileSystem.hpp>
 #include <galaxy/res/ShaderBook.hpp>
+#include <galaxy/ui/widgets/Image.hpp>
 
 #include <galaxy/core/ServiceLocator.hpp>
 
@@ -24,7 +25,7 @@ namespace sc
 		setName("GUILayer");
 
 		m_gui.construct(dt, SL_HANDLE.pool());
-		auto* theme = m_gui.addTheme();
+		auto* theme = m_gui.createTheme();
 
 		theme->setWindow(SL_HANDLE.window());
 		theme->setRenderer(SL_HANDLE.renderer());
@@ -45,11 +46,22 @@ namespace sc
 				auto file = galaxy::FileSystem::s_root + galaxy::FileSystem::s_textures + texture.get<std::string>();
 				theme->addTextureToAtlas(file);
 			});
-		
+
 			ifs.close();
 
 			theme->createTextureAtlas(*SL_HANDLE.shaderbook()->get("render_to_texture"));
 		}
+
+		auto* atlas = theme->getAtlas();
+		auto parent = atlas->getTexQuad("ui_oga");
+
+		atlas->defineCustomQuad("button_pressed", { parent.m_x + 353, parent.m_y + 302, 169, 32 });
+		atlas->defineCustomQuad("button_default", { parent.m_x + 353, parent.m_y + 346, 169, 32 });
+		atlas->defineCustomQuad("button_hovered", { parent.m_x + 354, parent.m_y + 390, 169, 32 });
+		atlas->defineCustomQuad("textbox", { parent.m_x + 19, parent.m_y + 866, 566, 153 });
+		atlas->defineCustomQuad("image", { parent.m_x + 31, parent.m_y + 75, 312, 287 });
+
+		m_gui.createWidget<galaxy::Image>("image", 10.0f, 10.0f);
 	}
 
 	GUILayer::~GUILayer() noexcept
