38f22713c409253119a3d6df9aea7dac2da40748
domre-desktop <dev.dom.re@gmail.com>
2020-07-17 09:43:56 +0800

    cpp20

--- galaxy/protostar/src/protostar/graphics/Colour.cpp
+++ galaxy/protostar/src/protostar/graphics/Colour.cpp
@@ -13,7 +13,12 @@
 namespace pr
 {
 	Colour::Colour() noexcept
-		:m_red(0), m_green(0), m_blue(0), m_alpha(255)
+		:m_red(255), m_green(255), m_blue(255), m_alpha(255)
+	{
+	}
+
+	Colour::Colour(const std::uint8_t r, const std::uint8_t g, const std::uint8_t b) noexcept
+		:m_red(r), m_green(g), m_blue(b), m_alpha(255)
 	{
 	}
 
@@ -22,77 +27,63 @@ namespace pr
 	{
 	}
 
-	std::array<float, 4> Colour::asFloats() noexcept
+	std::array<float, 4>&& Colour::getNormalized()
 	{
-		std::array<float, 4> arr = { static_cast<float>(m_red), static_cast<float>(m_green),
-			static_cast<float>(m_blue), static_cast<float>(m_alpha) };
-		return std::move(arr);
-	}
-
-	std::array<int, 4> Colour::asIntegers() noexcept
-	{
-		std::array<int, 4> arr = { static_cast<int>(m_red), static_cast<int>(m_green),
-			static_cast<int>(m_blue), static_cast<int>(m_alpha) };
-		return std::move(arr);
-	}
-
-	std::array<float, 4> Colour::asGLColour() noexcept
-	{
-		float r = 0.0f, g = 0.0f, b = 0.0f, a = 0.0f;
+		std::array<float, 4> arr = { 0.0f, 0.0f, 0.0f, 0.0f };
 
 		// Avoids floating precision errors.
 		if (m_red == 0)
 		{
-			r = 0.0f;
+			arr[0] = 0.0f;
 		}
 		else if (m_red == 255)
 		{
-			r = 1.0f;
+			arr[0] = 1.0f;
 		}
 		else
 		{
-			r = static_cast<float>(m_red) / static_cast<float>(0xFF);
+			arr[0] = static_cast<float>(m_red) / static_cast<float>(0xFF);
 		}
 
 		if (m_green == 0)
 		{
-			g = 0.0f;
+			arr[1] = 0.0f;
 		}
 		else if (m_green == 255)
 		{
-			g = 1.0f;
+			arr[1] = 1.0f;
 		}
 		else
 		{
-			g = static_cast<float>(m_green) / static_cast<float>(0xFF);
+			arr[1] = static_cast<float>(m_green) / static_cast<float>(0xFF);
 		}
 
 		if (m_blue == 0)
 		{
-			b = 0.0f;
+			arr[2] = 0.0f;
 		}
 		else if (m_blue == 255)
 		{
-			b = 1.0f;
+			arr[2] = 1.0f;
 		}
 		else
 		{
-			b = static_cast<float>(m_blue) / static_cast<float>(0xFF);
+			arr[2] = static_cast<float>(m_blue) / static_cast<float>(0xFF);
 		}
 
 		if (m_alpha == 0)
 		{
-			a = 0.0f;
+			arr[3] = 0.0f;
 		}
 		else if (m_alpha == 255)
 		{
-			a = 1.0f;
+			arr[3] = 1.0f;
 		}
 		else
 		{
-			a = static_cast<float>(m_alpha) / static_cast<float>(0xFF);
+			arr[3] = static_cast<float>(m_alpha) / static_cast<float>(0xFF);
 		}
 
-		return std::move(std::array<float, 4>{r, g, b, a});
+		return std::move(arr);
 	}
 }
--- galaxy/protostar/src/protostar/graphics/Colour.hpp
+++ galaxy/protostar/src/protostar/graphics/Colour.hpp
@@ -9,6 +9,9 @@
 #define PROTOSTAR_COLOUR_HPP_
 
 #include <array>
+#include <cstdint>
+
+#include "protostar/system/Concepts.hpp"
 
 ///
 /// Core namespace.
@@ -27,6 +30,15 @@ namespace pr
 		Colour() noexcept;
 
 		///
+		/// RGB constructor.
+		///
+		/// \param r Red.
+		/// \param g Green.
+		/// \param b Blue.
+		///
+		Colour(const std::uint8_t r, const std::uint8_t g, const std::uint8_t b) noexcept;
+
+		///
 		/// Argument constructor.
 		///
 		/// \param r Red.
@@ -42,27 +54,21 @@ namespace pr
 		~Colour() noexcept = default;
 
 		///
-		/// Converts to fixed size float array.
+		/// Converts to fixed size array of type Type.
 		///
-		/// \return Move invoked fixed size std::array of floats.
+		/// \return Moveable fixed size std::array of Type.
 		///
-		std::array<float, 4> asFloats() noexcept;
+		template<IsArithmetic Type>
+		[[nodiscard]] std::array<Type, 4>&& getAs();
 
 		///
-		/// Converts to fixed size int array.
-		///
-		/// \return Move invoked fixed size std::array of ints.
-		///
-		std::array<int, 4> asIntegers() noexcept;
-
-		///
-		/// \brief Converts to fixed size float array compatible with OpenGL
+		/// \brief Normalizes values and returns as floats.
 		///
 		/// Floats are in range 0.0f - 1.0f.
 		///
 		/// \return Move invoked fixed size std::array of floats.
 		///
-		std::array<float, 4> asGLColour() noexcept;
+		[[nodiscard]] std::array<float, 4>&& getNormalized();
 
 	public:
 		///
@@ -85,6 +91,20 @@ namespace pr
 		///
 		std::uint8_t m_alpha;
 	};
+
+	template<IsArithmetic Type>
+	inline std::array<Type, 4>&& Colour::getAs()
+	{
+		std::array<Type, 4> arr =
+		{ 
+			static_cast<Type>(m_red),
+			static_cast<Type>(m_green),
+			static_cast<Type>(m_blue),
+			static_cast<Type>(m_alpha)
+		};
+
+		return std::move(arr);
+	}
 }
 
 #endif
