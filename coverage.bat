96ad902bf1ff9ae7fdb7f7ebfa3add8e4f7a8f87
DomRe <dev.dom.re@gmail.com>
2020-08-16 19:06:06 +0800

    more ci

--- coverage.bat
+++ coverage.bat
@@ -1,3 +1,3 @@
 if "%CONFIGURATION%"=="Debug" (
-	OpenCppCoverage --sources c:\projects\galaxy --modules c:\projects\galaxy --export_type=cobertura:cobertura.xml -- C:\projects\galaxy\tests\bin\%CONFIGURATION%\%PLATFORM%\tests.exe & codecov --root .. -f cobertura.xml -t %CODECOV_TOKEN%
+	OpenCppCoverage --sources c:\projects\galaxy --modules c:\projects\galaxy --export_type=cobertura:cobertura.xml -- C:\projects\galaxy\output\bin\tests\tests.exe & codecov --root .. -f cobertura.xml -t %CODECOV_TOKEN%
 )
