c2a23a64e59338c977ade62289be27db31ec7a57
DomRe <dev.dom.re@gmail.com>
2020-08-28 23:06:58 +0800

    temp fix for thread waiting

--- galaxy/protostar/src/protostar/async/Task.cpp
+++ galaxy/protostar/src/protostar/async/Task.cpp
@@ -5,6 +5,8 @@
 /// Refer to LICENSE.txt for more details.
 ///
 
+#include <thread>
+
 #include "protostar/system/cpp20.hpp"
 #include STL_ATOMIC
 
@@ -38,7 +40,15 @@ namespace pr
 
 	void Task::wait_until_done()
 	{
-		std::atomic_wait(&m_done, false);
+		if (!m_done)
+		{
+			while (!m_done)
+			{
+				using namespace std::chrono_literals;
+				std::this_thread::sleep_for(1s);
+			}
+			//std::atomic_wait(&m_done, false);
+		}
 	}
 
 	bool Task::is_done() noexcept
