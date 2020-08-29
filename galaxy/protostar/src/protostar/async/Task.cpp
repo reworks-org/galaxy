8b3469ca5cbe4d4f88b2bd0b89c831664abbd8a2
DomRe <dev.dom.re@gmail.com>
2020-08-29 17:36:10 +0800

    atomic_wait

--- galaxy/protostar/src/protostar/async/Task.cpp
+++ galaxy/protostar/src/protostar/async/Task.cpp
@@ -5,8 +5,6 @@
 /// Refer to LICENSE.txt for more details.
 ///
 
-#include <thread>
-
 #include "protostar/system/cpp20.hpp"
 #include STL_ATOMIC
 
@@ -36,18 +34,14 @@ namespace pr
 	{
 		m_task();
 		m_done = true;
+		std::atomic_notify_one(&m_done);
 	}
 
 	void Task::wait_until_done()
 	{
 		if (!m_done)
 		{
-			while (!m_done)
-			{
-				using namespace std::chrono_literals;
-				std::this_thread::sleep_for(1s);
-			}
-			//std::atomic_wait(&m_done, false);
+			std::atomic_wait(&m_done, false);
 		}
 	}
 
