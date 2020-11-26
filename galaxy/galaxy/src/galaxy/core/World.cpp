dc1b267c99a4a003aaad9fb1f66722d1331eb32a
DomRe <dev.dom.re@gmail.com>
2020-11-26 12:24:43 +0800

    use json-utils

--- galaxy/galaxy/src/galaxy/core/World.cpp
+++ galaxy/galaxy/src/galaxy/core/World.cpp
@@ -11,6 +11,7 @@
 
 #include "galaxy/fs/FileSystem.hpp"
 #include "galaxy/components/All.hpp"
+#include "galaxy/scripting/JSONUtils.hpp"
 
 #include "World.hpp"
 
@@ -38,38 +39,25 @@ namespace galaxy
 	const sr::Entity World::create_from_json(std::string_view file)
 	{
 		// Makes sure the filepath is correct for the current platform.
-		sr::Entity entity = 0;
-		auto path         = std::filesystem::path {file};
-		std::ifstream ifs;
-		ifs.open(path.string(), std::ifstream::in);
+		sr::Entity entity   = 0;
+		entity              = create();
+		nlohmann::json root = galaxy::json::parse_from_disk(file);
 
-		if (!ifs.good())
+		// Loop over components
+		if (!root.empty())
 		{
-			PL_LOG(PL_ERROR, "Failed to open file: {0}.", path.string());
+			for (auto& [key, value] : root.items())
+			{
+				// Use the assign function to create components for entities without having to know the type.
+				m_component_factory[key](entity, value);
+			}
 		}
 		else
 		{
-			nlohmann::json root;
-
-			// Use JSON stream to deserialize data and parse.
-			ifs >> root;
-			entity = create();
-
-			// Loop over components
-			if (!root.empty())
-			{
-				for (auto& [key, value] : root.items())
-				{
-					// Use the assign function to create components for entities without having to know the type.
-					m_component_factory[key](entity, value);
-				}
-			}
-			else
-			{
-				PL_LOG(PL_WARNING, "Created an entity with no components.");
-			}
+			PL_LOG(PL_WARNING, "Created an entity with no components.");
 		}
 
 		return entity;
 	}
+
 } // namespace galaxy
