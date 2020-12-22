6c6a33a93bcbb6490101987e227d5af62f113c9b
DomRe <dev.dom.re@gmail.com>
2020-12-22 21:11:32 +0800

    update release build

--- cmake/LinkDefs.cmake
+++ cmake/LinkDefs.cmake
@@ -2,13 +2,13 @@ if (MSVC)
 	set(GALAXY_LINK_FLAGS
         /ignore:4099
         /MACHINE:X64
-        /SUBSYSTEM:CONSOLE
 	)
 
 	set(GALAXY_LINK_FLAGS_DEBUG
 		${GALAXY_LINK_FLAGS}
 		/INCREMENTAL
         /DEBUG
+        /SUBSYSTEM:CONSOLE
 	)
 
 	set(GALAXY_LINK_FLAGS_RELEASE
@@ -17,6 +17,8 @@ if (MSVC)
         /OPT:REF
         /OPT:ICF
         /LTCG:incremental
+        /SUBSYSTEM:WINDOWS
+        /ENTRY:mainCRTStartup
 	)
 else()
 	set(GALAXY_LINK_FLAGS
