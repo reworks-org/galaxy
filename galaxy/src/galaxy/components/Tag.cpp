2221bcd47d34320bd8156ca183ce85f70ca7e2a5
DomRe <dev.dom.re@gmail.com>
2021-02-16 17:20:03 +0800

    default tag value

--- galaxy/src/galaxy/components/Tag.cpp
+++ galaxy/src/galaxy/components/Tag.cpp
@@ -14,7 +14,7 @@ namespace galaxy
 	namespace components
 	{
 		Tag::Tag() noexcept
-		    : Serializable {this}, m_tag {""}
+		    : Serializable {this}, m_tag {"Default Tag"}
 		{
 		}
 
@@ -30,7 +30,7 @@ namespace galaxy
 		}
 
 		Tag::Tag(Tag&& t) noexcept
-		    : Serializable {this}, m_tag {""}
+		    : Serializable {this}, m_tag {"Default Tag"}
 		{
 			this->m_tag = std::move(t.m_tag);
 		}
