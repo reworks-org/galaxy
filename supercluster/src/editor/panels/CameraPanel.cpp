8037d02312c6de9be8298c189031b6850a76583d
DomRe <dev.dom.re@gmail.com>
2021-02-17 23:49:59 +0800

    create camerapanel

---
+++ supercluster/src/editor/panels/CameraPanel.cpp
@@ -0,0 +1,25 @@
+///
+/// CameraPanel.cpp
+/// supercluster
+///
+/// Refer to LICENSE.txt for more details.
+///
+
+#include <imgui/imgui_stdlib.h>
+
+#include "CameraPanel.hpp"
+
+namespace sc
+{
+	namespace panel
+	{
+		void CameraPanel::render()
+		{
+			if (ImGui::Begin("Camera Editor"))
+			{
+			}
+
+			ImGui::End();
+		}
+	} // namespace panel
+} // namespace sc
---
+++ supercluster/src/editor/panels/CameraPanel.hpp
@@ -0,0 +1,25 @@
+///
+/// CameraPanel.hpp
+/// supercluster
+///
+/// Refer to LICENSE.txt for more details.
+///
+
+#ifndef SUPERCLUSTER_EDITOR_PANELS_CAMERAPANEL_HPP_
+#define SUPERCLUSTER_EDITOR_PANELS_CAMERAPANEL_HPP_
+
+namespace sc
+{
+	namespace panel
+	{
+		class CameraPanel final
+		{
+		public:
+			void render();
+
+		private:
+		};
+	} // namespace panel
+} // namespace sc
+
+#endif
