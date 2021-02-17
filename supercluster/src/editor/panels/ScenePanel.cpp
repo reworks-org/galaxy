00d67b03e2a5f049a447738002ea9093624814bf
DomRe <dev.dom.re@gmail.com>
2021-02-17 15:55:51 +0800

    added scene panel

---
+++ supercluster/src/editor/panels/ScenePanel.cpp
@@ -0,0 +1,25 @@
+///
+/// ScenePanel.cpp
+/// supercluster
+///
+/// Refer to LICENSE.txt for more details.
+///
+
+#include <imgui/imgui_stdlib.h>
+
+#include "ScenePanel.hpp"
+
+namespace sc
+{
+	namespace panel
+	{
+		void ScenePanel::render()
+		{
+			if (ImGui::Begin("Scenes"))
+			{
+			}
+
+			ImGui::End();
+		}
+	} // namespace panel
+} // namespace sc
---
+++ supercluster/src/editor/panels/ScenePanel.hpp
@@ -0,0 +1,25 @@
+///
+/// ScenePanel.hpp
+/// supercluster
+///
+/// Refer to LICENSE.txt for more details.
+///
+
+#ifndef SUPERCLUSTER_EDITOR_PANELS_SCENEPANEL_HPP_
+#define SUPERCLUSTER_EDITOR_PANELS_SCENEPANEL_HPP_
+
+namespace sc
+{
+	namespace panel
+	{
+		class ScenePanel final
+		{
+		public:
+			void render();
+
+		private:
+		};
+	} // namespace panel
+} // namespace sc
+
+#endif
