2dcecb12ca67d3dda8e3c9c38f43e65daf915f7c
DomRe <dev.dom.re@gmail.com>
2021-02-23 16:34:49 +0800

    converted misc utility functions into one map utils header

---
+++ galaxy/src/galaxy/map/MapUtils.cpp
@@ -0,0 +1,47 @@
+///
+/// MapUtils.cpp
+/// galaxy
+///
+/// Refer to LICENSE.txt for more details.
+///
+
+#include "galaxy/error/Log.hpp"
+
+#include "MapUtils.hpp"
+
+namespace galaxy
+{
+	namespace map
+	{
+		graphics::Colour map::convert_colour(std::string_view hex_colour)
+		{
+			graphics::Colour colour;
+			auto str = static_cast<std::string>(hex_colour);
+
+			if (str[0] == '#')
+			{
+				str.erase(0, 1);
+			}
+
+			if (str.length() == 8)
+			{
+				if (std::sscanf(str.c_str(), "%02hhx%02hhx%02hhx%02hhx", &colour.m_alpha, &colour.m_red, &colour.m_green, &colour.m_blue) != 4)
+				{
+					GALAXY_LOG(GALAXY_FATAL, "Failed to fill 4 arguments for std::sscanf.");
+				}
+			}
+			else if (str.length() == 6)
+			{
+				if (std::sscanf(str.c_str(), "%02hhx%02hhx%02hhx", &colour.m_red, &colour.m_green, &colour.m_blue) != 3)
+				{
+					GALAXY_LOG(GALAXY_FATAL, "Failed to fill 3 arguments for std::sscanf.");
+				}
+
+				colour.m_alpha = 255;
+			}
+
+			return colour;
+		}
+
+	} // namespace map
+} // namespace galaxy
--- galaxy/src/galaxy/map/MapUtils.hpp
+++ galaxy/src/galaxy/map/MapUtils.hpp
@@ -1,19 +1,29 @@
 ///
-/// TiledProperty.hpp
+/// MapUtils.hpp
 /// galaxy
 ///
 /// Refer to LICENSE.txt for more details.
 ///
 
-#ifndef GALAXY_MAP_TILEDPROPERTY_HPP_
-#define GALAXY_MAP_TILEDPROPERTY_HPP_
+#ifndef GALAXY_MAP_MAPUTILS_HPP_
+#define GALAXY_MAP_MAPUTILS_HPP_
 
+#include "galaxy/graphics/Colour.hpp"
 #include "galaxy/meta/Concepts.hpp"
 
 namespace galaxy
 {
 	namespace map
 	{
+		///
+		/// Convert a hex string colour to a graphics colour object.
+		///
+		/// \param hex_colour String of hex colour i.e. AARRGGBB or RRGGBB.
+		///
+		/// \return Newly constructed graphics::Colour object.
+		///
+		graphics::Colour convert_colour(std::string_view hex_colour);
+
 		///
 		/// Concept for tiled property types.
 		///
