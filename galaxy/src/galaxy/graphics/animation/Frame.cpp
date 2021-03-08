57019e0fe098c6b839311678f3ed873619ccbf5f
DomRe <dev.dom.re@gmail.com>
2021-03-08 15:07:36 +0800

    fix incorrect order

--- galaxy/src/galaxy/graphics/animation/Frame.cpp
+++ galaxy/src/galaxy/graphics/animation/Frame.cpp
@@ -17,17 +17,17 @@ namespace galaxy
 	namespace graphics
 	{
 		Frame::Frame() noexcept
-		    : Serializable {this}, m_id {""}, m_region {0.0f, 0.0f, 0.0f, 0.0f}, m_time_per_frame {0.0}
+		    : Serializable {this}, m_time_per_frame {0.0}, m_id {""}, m_region {0.0f, 0.0f, 0.0f, 0.0f}
 		{
 		}
 
 		Frame::Frame(const graphics::fRect& region, const double time_per_frame) noexcept
-		    : Serializable {this}, m_id {""}, m_region {region}, m_time_per_frame {0.0}
+		    : Serializable {this}, m_time_per_frame {time_per_frame}, m_id {""}, m_region {region}
 		{
 		}
 
 		Frame::Frame(const nlohmann::json& json)
-		    : Serializable {this}, m_id {""}, m_time_per_frame {0.0}
+		    : Serializable {this}, m_time_per_frame {0.0}, m_id {""}, m_region {0.0f, 0.0f, 0.0f, 0.0f}
 		{
 			deserialize(json);
 		}
