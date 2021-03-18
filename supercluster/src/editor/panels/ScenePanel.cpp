02a0a359796872996b6eddb33651951a662fe62d
DomRe <dev.dom.re@gmail.com>
2021-03-18 13:23:51 +0800

    select scene camera, not top scene camera

--- supercluster/src/editor/panels/ScenePanel.cpp
+++ supercluster/src/editor/panels/ScenePanel.cpp
@@ -52,11 +52,6 @@ namespace sc
 						flags |= ImGuiTreeNodeFlags_Selected;
 					}
 
-					if (m_selected.empty() || m_selected != name)
-					{
-						ImGui::SetNextItemOpen(false);
-					}
-
 					const bool is_open = ImGui::TreeNodeEx(name.c_str(), flags);
 					if (ImGui::IsItemClicked())
 					{
@@ -78,50 +73,50 @@ namespace sc
 
 						ImGui::Text("Move");
 
-						float s_move_x = scene_stack.top()->m_camera.get_pos().x;
+						float s_move_x = scene->m_camera.get_pos().x;
 						ImGui::SetNextItemWidth(INPUT_WIDTH);
 						if (ImGui::InputFloat("X##01", &s_move_x, 1.0f, 10.0f, "%.1f", ImGuiInputTextFlags_CharsNoBlank | ImGuiInputTextFlags_EnterReturnsTrue))
 						{
-							scene_stack.top()->m_camera.move_x(s_move_x);
+							scene->m_camera.move_x(s_move_x);
 						}
 
 						ImGui::SameLine();
 
-						float s_move_y = scene_stack.top()->m_camera.get_pos().y;
+						float s_move_y = scene->m_camera.get_pos().y;
 						ImGui::SetNextItemWidth(INPUT_WIDTH);
 						if (ImGui::InputFloat("Y##02", &s_move_y, 1.0f, 10.0f, "%.1f", ImGuiInputTextFlags_CharsNoBlank | ImGuiInputTextFlags_EnterReturnsTrue))
 						{
-							scene_stack.top()->m_camera.move_y(s_move_y);
+							scene->m_camera.move_y(s_move_y);
 						}
 
-						int s_speed = static_cast<int>(scene_stack.top()->m_camera.get_speed());
+						int s_speed = static_cast<int>(scene->m_camera.get_speed());
 						if (ImGui::InputInt("Speed##03", &s_speed, 1, 10, ImGuiInputTextFlags_CharsNoBlank | ImGuiInputTextFlags_EnterReturnsTrue))
 						{
-							scene_stack.top()->m_camera.set_speed(s_speed);
+							scene->m_camera.set_speed(s_speed);
 						}
 
-						float s_zoom = scene_stack.top()->m_camera.get_scale();
+						float s_zoom = scene->m_camera.get_scale();
 						if (ImGui::SliderFloat("Scale##04", &s_zoom, 0.2, 10.0, "%.1f", ImGuiSliderFlags_AlwaysClamp | ImGuiSliderFlags_ClampOnInput))
 						{
-							scene_stack.top()->m_camera.zoom(s_zoom);
+							scene->m_camera.zoom(s_zoom);
 						}
 
 						ImGui::Text("Projection");
 
-						float s_proj_x = scene_stack.top()->m_camera.get_width();
+						float s_proj_x = scene->m_camera.get_width();
 						ImGui::SetNextItemWidth(INPUT_WIDTH);
 						if (ImGui::InputFloat("X##05", &s_proj_x, 1.0f, 10.0f, "%.1f", ImGuiInputTextFlags_CharsNoBlank | ImGuiInputTextFlags_EnterReturnsTrue))
 						{
-							scene_stack.top()->m_camera.set_width(s_proj_x);
+							scene->m_camera.set_width(s_proj_x);
 						}
 
 						ImGui::SameLine();
 
-						float s_proj_y = scene_stack.top()->m_camera.get_height();
+						float s_proj_y = scene->m_camera.get_height();
 						ImGui::SetNextItemWidth(INPUT_WIDTH);
 						if (ImGui::InputFloat("Y##06", &s_proj_y, 1.0f, 10.0f, "%.1f", ImGuiInputTextFlags_CharsNoBlank | ImGuiInputTextFlags_EnterReturnsTrue))
 						{
-							scene_stack.top()->m_camera.set_height(s_proj_y);
+							scene->m_camera.set_height(s_proj_y);
 						}
 
 						ImGui::Spacing();
