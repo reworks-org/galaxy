6deada89b7cf98e8b805fb7aa812a19971f546c2
DomRe <dev.dom.re@gmail.com>
2021-05-05 17:40:21 +0800

    improve debug output

--- galaxy/src/galaxy/core/Window.cpp
+++ galaxy/src/galaxy/core/Window.cpp
@@ -267,7 +267,7 @@ namespace galaxy
 							nullptr);
 							// clang-format on
 
-							glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DEBUG_SEVERITY_NOTIFICATION, 0, NULL, GL_FALSE);
+							glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
 						}
 
 						// Configure global GL state.
