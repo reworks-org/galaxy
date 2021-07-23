2ce7a7e1bba5fa84025d5f9eca3475b0c91c707e
DomRe <dev.dom.re@gmail.com>
2021-07-23 19:45:44 +0800

    fix compilation error

--- dependencies/imgui/imgui/addons/TextEditor.h
+++ dependencies/imgui/imgui/addons/TextEditor.h
@@ -73,8 +73,6 @@ namespace ImGui
 			Coordinates() : mLine(0), mColumn(0) {}
 			Coordinates(int aLine, int aColumn) : mLine(aLine), mColumn(aColumn)
 			{
-				assert(aLine >= 0);
-				assert(aColumn >= 0);
 			}
 			static Coordinates Invalid() { static Coordinates invalid(-1, -1); return invalid; }
 
