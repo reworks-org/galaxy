45205d7f7059e708fa35b33626e2186438f1ac43
DomRe <dev.dom.re@gmail.com>
2021-08-01 21:06:21 +0800

    more imgui helper functions

--- galaxy/src/galaxy/ui/ImGuiHelpers.cpp
+++ galaxy/src/galaxy/ui/ImGuiHelpers.cpp
@@ -5,4 +5,54 @@
 /// Refer to LICENSE.txt for more details.
 ///
 
+#include "galaxy/core/ServiceLocator.hpp"
+#include "galaxy/core/Window.hpp"
+
 #include "ImGuiHelpers.hpp"
+
+namespace galaxy
+{
+	namespace ui
+	{
+		ImGuiIO& imgui_init_context() noexcept
+		{
+			// clang-format off
+			IMGUI_CHECKVERSION();
+			ImGui::CreateContext();
+			ImGuiIO& io = ImGui::GetIO(); (void)io;
+			io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+			io.ConfigDockingAlwaysTabBar = true;
+			// clang-format on
+
+			ImGui_ImplGlfw_InitForOpenGL(SL_HANDLE.window()->gl_window(), true);
+			ImGui_ImplOpenGL3_Init("#version 450 core");
+
+			return io;
+		}
+
+		void imgui_new_frame() noexcept
+		{
+			ImGui_ImplOpenGL3_NewFrame();
+			ImGui_ImplGlfw_NewFrame();
+			ImGui::NewFrame();
+
+			auto* const imgui_viewport = ImGui::GetMainViewport();
+			ImGui::SetNextWindowPos(imgui_viewport->WorkPos);
+			ImGui::SetNextWindowSize(imgui_viewport->WorkSize);
+			ImGui::SetNextWindowViewport(imgui_viewport->ID);
+		}
+
+		void imgui_render() noexcept
+		{
+			ImGui::Render();
+			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		}
+
+		void imgui_destroy_context() noexcept
+		{
+			ImGui_ImplOpenGL3_Shutdown();
+			ImGui_ImplGlfw_Shutdown();
+			ImGui::DestroyContext();
+		}
+	} // namespace ui
+} // namespace galaxy
--- galaxy/src/galaxy/ui/ImGuiHelpers.hpp
+++ galaxy/src/galaxy/ui/ImGuiHelpers.hpp
@@ -8,49 +8,65 @@
 #ifndef GALAXY_UI_IMGUIHELPERS_HPP_
 #define GALAXY_UI_IMGUIHELPERS_HPP_
 
-#include <glm/vec3.hpp>
+#include <imgui/imgui_impl_opengl3.h>
+#include <imgui/imgui_impl_glfw.h>
 #include <imgui/imgui_stdlib.h>
-
-#include "galaxy/meta/Concepts.hpp"
 
 namespace galaxy
 {
 	namespace ui
 	{
-		template<meta::is_class Type>
-		[[maybe_unused]] const bool im_draw_class(Type& value, const char* label = "");
-
-		template<meta::is_class Type>
-		const bool im_draw_class(Type& value, const char* label)
-		{
-			static_assert("Unsupported class type.");
-			return false;
-		}
-
-		template<>
-		const bool im_draw_class(glm::vec3& value, const char* label)
-		{
-			int clicked = 0;
-
-			if (std::strlen(label) > 0)
-			{
-				ImGui::Text(label);
-			}
-
-			ImGui::SetNextItemWidth(60.0f);
-			clicked += ImGui::InputFloat("X", &value.x, 0.1f, 1.0f, "%.1f", ImGuiInputTextFlags_AutoSelectAll | ImGuiInputTextFlags_CharsNoBlank);
-			ImGui::SameLine();
-
-			ImGui::SetNextItemWidth(60.0f);
-			clicked += ImGui::InputFloat("Y", &value.y, 0.1f, 1.0f, "%.1f", ImGuiInputTextFlags_AutoSelectAll | ImGuiInputTextFlags_CharsNoBlank);
-			ImGui::SameLine();
-
-			ImGui::SetNextItemWidth(60.0f);
-			clicked += ImGui::InputFloat("Z", &value.z, 0.1f, 1.0f, "%.1f", ImGuiInputTextFlags_AutoSelectAll | ImGuiInputTextFlags_CharsNoBlank);
-
-			return static_cast<bool>(clicked);
-		}
+		[[maybe_unused]] ImGuiIO& imgui_init_context() noexcept;
+		void imgui_new_frame() noexcept;
+		void imgui_render() noexcept;
+		void imgui_destroy_context() noexcept;
 	} // namespace ui
 } // namespace galaxy
+
+[[nodiscard]] inline const bool operator==(const ImVec2& a, const ImVec2& b) noexcept
+{
+	return ((a.x == b.x) && (a.y == b.y));
+}
+
+[[nodiscard]] inline const bool operator!=(const ImVec2& a, const ImVec2& b) noexcept
+{
+	return !(a == b);
+}
+
+/*
+  template<meta::is_class Type>
+  [[maybe_unused]] const bool im_draw_class(Type& value, const char* label = "");
+
+  template<meta::is_class Type>
+  const bool im_draw_class(Type& value, const char* label)
+  {
+  static_assert("Unsupported class type.");
+  return false;
+  }
+
+  template<>
+  const bool im_draw_class(glm::vec3& value, const char* label)
+  {
+  int clicked = 0;
+
+  if (std::strlen(label) > 0)
+  {
+  ImGui::Text(label);
+  }
+
+  ImGui::SetNextItemWidth(60.0f);
+  clicked += ImGui::InputFloat("X", &value.x, 0.1f, 1.0f, "%.1f", ImGuiInputTextFlags_AutoSelectAll | ImGuiInputTextFlags_CharsNoBlank);
+  ImGui::SameLine();
+
+  ImGui::SetNextItemWidth(60.0f);
+  clicked += ImGui::InputFloat("Y", &value.y, 0.1f, 1.0f, "%.1f", ImGuiInputTextFlags_AutoSelectAll | ImGuiInputTextFlags_CharsNoBlank);
+  ImGui::SameLine();
+
+  ImGui::SetNextItemWidth(60.0f);
+  clicked += ImGui::InputFloat("Z", &value.z, 0.1f, 1.0f, "%.1f", ImGuiInputTextFlags_AutoSelectAll | ImGuiInputTextFlags_CharsNoBlank);
+
+  return static_cast<bool>(clicked);
+  }
+  */
 
 #endif
