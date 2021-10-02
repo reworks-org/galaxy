96781c676626710abd6761b4d37f9b890aa76084
Dom <dev.dom.re@gmail.com>
2021-10-02 23:56:04 +0800

    astar outline

---
+++ galaxy/src/galaxy/ai/AStar.hpp
@@ -0,0 +1,22 @@
+///
+/// AStar.hpp
+/// galaxy
+///
+/// Refer to LICENSE.txt for more details.
+///
+
+#ifndef GALAXY_AI_ASTAR_HPP_
+#define GALAXY_AI_ASTAR_HPP_
+
+#include <vector>
+
+// https://www.redblobgames.com/pathfinding/a-star/introduction.html
+
+namespace galaxy
+{
+	namespace ai
+	{
+	} // namespace ai
+} // namespace galaxy
+
+#endif
