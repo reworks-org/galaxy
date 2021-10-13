b1c80232041efc7ac7e8e7355cd5617e19957f68
Dom <dev.dom.re@gmail.com>
2021-10-13 17:51:06 +0800

    fix release build

--- scripts/build-external.cmd
+++ scripts/build-external.cmd
@@ -1,7 +1,7 @@
 cmake -G "Visual Studio 16 2019" -DCMAKE_BUILD_TYPE=%2 -DGALAXY_BUILD_TESTS=OFF -B%1build -H%1
-msbuild %1build\dependencies\efsw\efsw.sln
-msbuild %1build\dependencies\glfw3\GLFW.sln
-msbuild %1build\dependencies\openal\OpenAL.sln
-msbuild %1build\dependencies\zlib-ng\zlib.sln
+msbuild %1build\dependencies\efsw\efsw.sln /p:Configuration=%2
+msbuild %1build\dependencies\glfw3\GLFW.sln /p:Configuration=%2
+msbuild %1build\dependencies\openal\OpenAL.sln /p:Configuration=%2
+msbuild %1build\dependencies\zlib-ng\zlib.sln /p:Configuration=%2
 robocopy %1build\dependencies\zlib-ng %1dependencies\zconf zconf.h /mt /e
 exit 0
