7803557ebe05d84f1e9c514f78c53fb644335bc8
Dom <dev.dom.re@gmail.com>
2022-05-01 15:53:52 +0800

    restore key_repeat

--- galaxy/src/galaxy/core/Window.cpp
+++ galaxy/src/galaxy/core/Window.cpp
@@ -146,6 +146,19 @@ namespace galaxy
 								}
 								break;
 
+								case GLFW_REPEAT:
+								{
+									events::KeyRepeat kr
+									{
+										.m_keycode = static_cast<input::Keys>(key),
+										.m_mod = static_cast<input::InputMods>(mods),
+										.m_scancode = scancode
+									};
+								
+									win->m_event_queue.emplace<events::KeyRepeat>(std::move(kr));
+								}
+								break;
+
 								case GLFW_RELEASE:
 								{
 									events::KeyUp ku
@@ -538,76 +551,3 @@ namespace galaxy
 		}
 	} // namespace core
 } // namespace galaxy
-
-/*
-
-
-
-
-		const bool Window::key_down(input::Keys key) noexcept
-		{
-			return glfwGetKey(m_window, m_keyboard.m_keymap[key]) == GLFW_PRESS;
-		}
-
-		const bool Window::key_pressed(input::Keys key) noexcept
-		{
-			bool      res   = false;
-			const int state = glfwGetKey(m_window, m_keyboard.m_keymap[key]);
-			if (m_keyboard.m_prev_key_states[key] == GLFW_RELEASE && state == GLFW_PRESS)
-			{
-				res = true;
-			}
-
-			m_keyboard.m_prev_key_states[key] = state;
-			return res;
-		}
-
-		const bool Window::mouse_button_pressed(input::MouseButtons mouse_button) noexcept
-		{
-			bool res = false;
-
-			const int state = glfwGetMouseButton(m_window, m_mouse.m_mouse_map[mouse_button]);
-			if (state == GLFW_PRESS && m_mouse.m_prev_mouse_btn_states[m_mouse.m_mouse_map[mouse_button]] == GLFW_RELEASE)
-			{
-				res = true;
-			}
-
-			m_mouse.m_prev_mouse_btn_states[m_mouse.m_mouse_map[mouse_button]] = state;
-
-			return res;
-		}
-
-		const bool Window::mouse_button_released(input::MouseButtons mouse_button) noexcept
-		{
-			if (glfwGetMouseButton(m_window, m_mouse.m_mouse_map[mouse_button]) == GLFW_RELEASE)
-			{
-				m_mouse.m_prev_mouse_btn_states[m_mouse.m_mouse_map[mouse_button]] = GLFW_RELEASE;
-				return true;
-			}
-
-			return false;
-		}
-
-
-
-		const double Window::get_scroll_delta() noexcept
-		{
-			const double old_delta = m_mouse.m_scroll_delta;
-			m_mouse.m_scroll_delta = 0.0;
-
-			return old_delta;
-		}
-
-		glm::vec2 Window::get_cursor_pos() noexcept
-		{
-			glfwGetCursorPos(m_window, &m_cursor.m_pos.x, &m_cursor.m_pos.y);
-			return m_cursor.m_pos;
-		}
-
-
-
-		const glm::vec2& Window::cursor_size() const noexcept
-		{
-			return m_cursor.m_cursor_size;
-		}
-*/
