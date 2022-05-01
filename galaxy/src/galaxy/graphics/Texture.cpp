de49c2ba94ab8b0f1f1bc6a51c532d4ca32a0c38
Dom <dev.dom.re@gmail.com>
2022-05-01 14:36:13 +0800

    restrict to png

--- galaxy/src/galaxy/graphics/Texture.cpp
+++ galaxy/src/galaxy/graphics/Texture.cpp
@@ -77,38 +77,45 @@ namespace galaxy
 			auto file_info = fs.find(file);
 			if (file_info.m_code == fs::FileInfo::Code::FOUND)
 			{
-				stbi_set_flip_vertically_on_load(true);
-				unsigned char* data = stbi_load(file_info.m_string.c_str(), &m_width, &m_height, nullptr, STBI_rgb_alpha);
-
-				if (data)
+				if (file_info.m_path.extension() == "png")
 				{
-					glTextureStorage2D(m_texture, 1, GL_RGBA8, m_width, m_height);
-					glTextureSubImage2D(m_texture, 0, 0, 0, m_width, m_height, GL_RGBA, GL_UNSIGNED_BYTE, data);
-					glGenerateTextureMipmap(m_texture);
+					stbi_set_flip_vertically_on_load(true);
+					unsigned char* data = stbi_load(file_info.m_string.c_str(), &m_width, &m_height, nullptr, STBI_rgb_alpha);
 
-					if (config.get<int>("trilinear_filtering", "graphics"))
+					if (data)
 					{
-						glTextureParameteri(m_texture, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-						glTextureParameteri(m_texture, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+						glTextureStorage2D(m_texture, 1, GL_RGBA8, m_width, m_height);
+						glTextureSubImage2D(m_texture, 0, 0, 0, m_width, m_height, GL_RGBA, GL_UNSIGNED_BYTE, data);
+						glGenerateTextureMipmap(m_texture);
+
+						if (config.get<int>("trilinear_filtering", "graphics"))
+						{
+							glTextureParameteri(m_texture, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+							glTextureParameteri(m_texture, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+						}
+						else
+						{
+							glTextureParameteri(m_texture, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+							glTextureParameteri(m_texture, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+						}
+
+						glTextureParameteri(m_texture, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+						glTextureParameteri(m_texture, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+						glTextureParameterf(m_texture, GL_TEXTURE_MAX_ANISOTROPY, static_cast<float>(config.get<int>("ansiotrophic_filtering", "graphics")));
+
+						result = true;
 					}
 					else
 					{
-						glTextureParameteri(m_texture, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
-						glTextureParameteri(m_texture, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+						GALAXY_LOG(GALAXY_ERROR, "Failed to load texture file '{0}' because '{1}'.", file, stbi_failure_reason());
 					}
 
-					glTextureParameteri(m_texture, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-					glTextureParameteri(m_texture, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-					glTextureParameterf(m_texture, GL_TEXTURE_MAX_ANISOTROPY, static_cast<float>(config.get<int>("ansiotrophic_filtering", "graphics")));
-
-					result = true;
+					stbi_image_free(data);
 				}
 				else
 				{
-					GALAXY_LOG(GALAXY_ERROR, "Failed to load texture file '{0}' because '{1}'.", file, stbi_failure_reason());
+					GALAXY_LOG(GALAXY_ERROR, "Only PNG textures are supported, invalid texture '{0}'.", file);
 				}
-
-				stbi_image_free(data);
 			}
 			else
 			{
