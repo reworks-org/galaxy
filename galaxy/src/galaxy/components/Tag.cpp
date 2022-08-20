2c78b61cbab1f1749baf5ed96e8425f1bd3e3153
Dom <dev.dom.re@gmail.com>
2022-08-20 16:20:55 +0800

    tag component

---
+++ galaxy/src/galaxy/components/Tag.cpp
@@ -0,0 +1,64 @@
+///
+/// Tag.cpp
+/// galaxy
+///
+/// Refer to LICENSE.txt for more details.
+///
+
+#include <nlohmann/json.hpp>
+
+#include "Tag.hpp"
+
+namespace galaxy
+{
+	namespace components
+	{
+		Tag::Tag() noexcept
+			: Serializable {}
+			, m_tag {""}
+		{
+		}
+
+		Tag::Tag(std::string_view tag) noexcept
+			: Serializable {}
+			, m_tag {tag}
+		{
+		}
+
+		Tag::Tag(const nlohmann::json& json)
+			: Serializable {}
+		{
+			deserialize(json);
+		}
+
+		Tag::Tag(Tag&& t) noexcept
+			: Serializable {}
+			, m_tag {"null"}
+		{
+			this->m_tag = std::move(t.m_tag);
+		}
+
+		Tag& Tag::operator=(Tag&& t) noexcept
+		{
+			if (this != &t)
+			{
+				this->m_tag = std::move(t.m_tag);
+			}
+
+			return *this;
+		}
+
+		nlohmann::json Tag::serialize()
+		{
+			nlohmann::json json = "{}"_json;
+			json["tag"]         = m_tag;
+
+			return json;
+		}
+
+		void Tag::deserialize(const nlohmann::json& json)
+		{
+			m_tag = json.at("tag");
+		}
+	} // namespace components
+} // namespace galaxy
