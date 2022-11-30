e1775330717d9d925b2f28696eb4375bfd22eebb
Dom <dev.dom.re@gmail.com>
2022-11-30 15:33:04 +0800

    only compress data in release mode

--- supercluster/src/layers/Editor.cpp
+++ supercluster/src/layers/Editor.cpp
@@ -186,11 +186,15 @@ namespace sc
 			const auto fs_path     = std::filesystem::path(path);
 			m_current_project_path = fs_path.string();
 
-			auto decompressed   = std::string(buffer.begin(), buffer.end());
-			auto decoded_zlib   = algorithm::decode_zlib(decompressed);
-			auto decoded_base64 = algorithm::decode_base64(decoded_zlib);
+#ifdef _DEBUG
+			auto data = std::string(buffer.begin(), buffer.end());
+#elif
+			auto data = std::string(buffer.begin(), buffer.end());
+			data      = algorithm::decode_zlib(data);
+			data      = algorithm::decode_base64(data);
+#endif
 
-			auto json = json::parse_from_mem(decoded_base64);
+			auto json = json::parse_from_mem(data);
 
 			if (json.has_value())
 			{
@@ -231,11 +235,15 @@ namespace sc
 
 			if (ofs.good())
 			{
-				auto data           = m_project_scenes.serialize().dump(4);
-				auto encoded_base64 = algorithm::encode_base64(data);
-				auto encoded_zlib   = algorithm::encode_zlib(encoded_base64);
+#ifdef _DEBUG
+				auto data = m_project_scenes.serialize().dump(4);
+#elif
+                auto data = m_project_scenes.serialize().dump(4);
+                data      = algorithm::encode_base64(data);
+                data      = algorithm::encode_zlib(data);
+#endif
 
-				ofs.write(encoded_zlib.data(), encoded_zlib.size());
+				ofs.write(data.data(), data.size());
 				ofs.close();
 
 				core::ServiceLocator<core::Window>::ref().set_title(std::filesystem::path(m_current_project_path).stem().string().c_str());
