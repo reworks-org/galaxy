d8db2dad3299ee67674c44067cacd266da8e6c04
Dom <dev.dom.re@gmail.com>
2022-12-08 13:55:57 +0800

    update strutils lua + tests

--- galaxy/src/galaxy/scripting/Lua.cpp
+++ galaxy/src/galaxy/scripting/Lua.cpp
@@ -1021,8 +1021,16 @@ namespace galaxy
 			lua.set("GALAXY_REMOVE_LOG_FILES_OLDER_THAN_HOURS", GALAXY_REMOVE_LOG_FILES_OLDER_THAN_HOURS);
 			lua.set("GALAXY_APP_DIR", GALAXY_APP_DIR);
 
-			lua.set_function("str_split", &strutils::split);
+			/*
 			lua.set_function("str_begins_with", &strutils::begins_with);
+			lua.set_function("str_ltrim", &strutils::ltrim);
+			lua.set_function("str_make_single_spaced", &strutils::make_single_spaced);
+			lua.set_function("str_replace_all", &strutils::replace_all);
+			lua.set_function("str_replace_first", &strutils::replace_first);
+			lua.set_function("str_rtrim", &strutils::rtrim);
+			lua.set_function("str_split", &strutils::split);
+			lua.set_function("str_trim", &strutils::trim);
+			*/
 		}
 
 		void inject_services_into_lua()
--- tests/src/utils/StringUtilsTest.cpp
+++ tests/src/utils/StringUtilsTest.cpp
@@ -26,14 +26,63 @@ TEST(Utils, ReplaceFirst)
 {
 	std::string test = "123.456";
 
-	EXPECT_EQ(galaxy::strutils::replace_first(test, "123", "456"), "456.456");
-	EXPECT_EQ(galaxy::strutils::replace_first(test, "456", "123"), "123.456");
-	EXPECT_EQ(galaxy::strutils::replace_first(test, "456", "123"), "123.123");
+	galaxy::strutils::replace_first(test, "123", "456");
+	EXPECT_EQ(test, "456.456");
+
+	galaxy::strutils::replace_first(test, "456", "123");
+	EXPECT_EQ(test, "123.456");
+
+	galaxy::strutils::replace_first(test, "456", "123");
+	EXPECT_EQ(test, "123.123");
 }
 
 TEST(Utils, ReplaceAll)
 {
 	std::string test = "123.456.123";
-	EXPECT_EQ(galaxy::strutils::replace_all(test, "123", "456"), "456.456.456");
-	EXPECT_EQ(galaxy::strutils::replace_all(test, "456", "123"), "123.123.123");
+
+	galaxy::strutils::replace_all(test, "123", "456");
+	EXPECT_EQ(test, "456.456.456");
+
+	galaxy::strutils::replace_all(test, "456", "123");
+	EXPECT_EQ(test, "123.123.123");
+}
+
+TEST(Utils, BeginsWith)
+{
+	std::string test = "testFooBar";
+
+	EXPECT_TRUE(galaxy::strutils::begins_with(test, "test"));
+	EXPECT_FALSE(galaxy::strutils::begins_with(test, "Bar"));
+}
+
+TEST(Utils, RTrim)
+{
+	std::string test = "  test    ";
+
+	galaxy::strutils::rtrim(test);
+	EXPECT_EQ(test, "test    ");
+}
+
+TEST(Utils, LTrim)
+{
+	std::string test = "  test    ";
+
+	galaxy::strutils::rtrim(test);
+	EXPECT_EQ(test, "  test");
+}
+
+TEST(Utils, Trim)
+{
+	std::string test = "  test    ";
+
+	galaxy::strutils::rtrim(test);
+	EXPECT_EQ(test, "test");
+}
+
+TEST(Utils, MakeSingleSpaced)
+{
+	std::string test = "t  e  s  t";
+
+	galaxy::strutils::make_single_spaced(test);
+	EXPECT_EQ(test, "t e s t");
 }
