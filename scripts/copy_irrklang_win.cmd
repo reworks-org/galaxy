80c5195c90f256ea691bc1620ca0e9725f9e72ee
Dom <dev.dom.re@gmail.com>
2023-03-26 16:32:42 +0800

    fix copy location

--- scripts/copy_irrklang_win.cmd
+++
@@ -1,2 +0,0 @@
-robocopy %1lib\irrKlang\bin %3\bin\%2 irrKlang.dll /mt /e
-exit 0
--- scripts/copy_tools_win.cmd
+++ scripts/copy_tools_win.cmd
@@ -1,2 +1,2 @@
-robocopy %1tools\tiled %2\assets\editor_data\tools\tiled /mt /e
+robocopy %1tools\tiled %2\bin\%3\assets\editor_data\tools\tiled /mt /e
 exit 0
