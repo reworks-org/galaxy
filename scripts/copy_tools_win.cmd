f777c065a6e03878789ef2e91e799e0de4313ba8
Dom <dev.dom.re@gmail.com>
2023-10-16 21:56:05 +0800

    dont extract existing files

--- scripts/copy_tools_win.cmd
+++ scripts/copy_tools_win.cmd
@@ -1,5 +1,5 @@
 robocopy %1tools\LDtk %2\bin\%3\editor_data\tools\LDtk /mt /e
 robocopy %1tools\LDtk\settings %AppData%\LDtk\settings /mt /e
 robocopy %1tools\bfxr %2\bin\%3\editor_data\tools\bfxr /mt /e
-%1tools\7zip\7za.exe e %2\bin\%3\editor_data\tools\LDtk\LDtk.7z -o%2\bin\%3\editor_data\tools\LDtk\
+%1tools\7zip\7za.exe e -aos %2\bin\%3\editor_data\tools\LDtk\LDtk.7z -o%2\bin\%3\editor_data\tools\LDtk\
 exit 0
