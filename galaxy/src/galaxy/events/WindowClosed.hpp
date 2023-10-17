4aec42739b23cf14ddd2f8b5a084de02253823dd
Dom <dev.dom.re@gmail.com>
2023-10-17 16:58:00 +0800

    fix header file being used as a source file

--- galaxy/src/galaxy/events/WindowClosed.hpp
+++ galaxy/src/galaxy/events/WindowClosed.hpp
@@ -5,8 +5,8 @@
 /// Refer to LICENSE.txt for more details.
 ///
 
-#ifndef GALAXY_EVENTS_WINDOCLOSED_HPP_
-#define GALAXY_EVENTS_WINDOCLOSED_HPP_
+#ifndef GALAXY_EVENTS_WINDOWCLOSED_HPP_
+#define GALAXY_EVENTS_WINDOWCLOSED_HPP_
 
 namespace galaxy
 {
@@ -15,7 +15,9 @@ namespace galaxy
 		///
 		/// Blank "Tag" class/type to signal the window is being closed.
 		///
+		// clang-format off
 		struct WindowClosed {};
+		// clang-format on
 	} // namespace events
 } // namespace galaxy
 
