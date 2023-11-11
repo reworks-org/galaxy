4e7cddea0fda9d3915759fe46f6c5456ec79daae
Dom <dev.dom.re@gmail.com>
2023-11-11 16:35:03 +0800

    update ldtk

--- scripts/copy_tools_win.cmd
+++ scripts/copy_tools_win.cmd
@@ -1,5 +1,4 @@
 robocopy %1tools\LDtk %2\bin\%3\editor\tools\LDtk /mt /e
-robocopy %1tools\LDtk\settings %AppData%\LDtk\settings /mt /e
 robocopy %1tools\bfxr %2\bin\%3\editor\tools\bfxr /mt /e
 %1tools\7zip\7za.exe e -aos %2\bin\%3\editor\tools\LDtk\LDtk.7z -o%2\bin\%3\editor\tools\LDtk\
 exit 0
