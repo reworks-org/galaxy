f01b2d5f2f621b0b71d41faba79785223d160454
Dom <dev.dom.re@gmail.com>
2024-02-01 23:58:06 +0800

    switch to custom entt

---
+++ lib/entt/src/entt/fwd.hpp
@@ -0,0 +1,9 @@
+// IWYU pragma: begin_exports
+#include "container/fwd.hpp"
+#include "core/fwd.hpp"
+#include "entity/fwd.hpp"
+#include "graph/fwd.hpp"
+#include "meta/fwd.hpp"
+#include "process/fwd.hpp"
+#include "signal/fwd.hpp"
+// IWYU pragma: end_exports
