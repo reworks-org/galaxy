9bf025122eb9350d80cf72497227871057fa6b0f
reworks <dev.dom.re@gmail.com>
2024-04-05 12:59:54 +0800

    add missing comments

--- galaxy/src/galaxy/scripting/Lua.hpp
+++ galaxy/src/galaxy/scripting/Lua.hpp
@@ -18,87 +18,87 @@ namespace galaxy
 		void inject();
 
 		///
-		///
+		/// Inject miniaudio into lua.
 		///
 		void inject_miniaudio();
 
 		///
-		///
+		/// Injects GLM into lua.
 		///
 		void inject_glm();
 
 		///
-		///
+		/// Injects EnTT into lua.
 		///
 		void inject_entt();
 
 		///
-		///
+		/// Injects Box2D into lua.
 		///
 		void inject_box2d();
 
 		///
-		/// Load nuklear ui into lua.
+		/// Inject Nuklear into Lua.
 		///
 		void inject_nuklear();
 
 		///
-		///
+		/// Inject galaxy components into lua.
 		///
 		void inject_components();
 
 		///
-		///
+		/// Inject galaxy core services into lua.
 		///
 		void inject_core();
 
 		///
-		///
+		/// Injects error handling into lua.
 		///
 		void inject_error();
 
 		///
-		///
+		/// Injects galaxy events into lua.
 		///
 		void inject_events();
 
 		///
-		///
+		/// Injects galaxy flag components into lua.
 		///
 		void inject_flags();
 
 		///
-		///
+		/// Injects VFS support into lua.
 		///
 		void inject_fs();
 
 		///
-		///
+		/// Injects graphics primitives, not routines, into lua.
 		///
 		void inject_graphics();
 
 		///
-		///
+		/// Injects input handling into lua.
 		///
 		void inject_input();
 
 		///
-		///
+		/// Injects math stuff into lua.
 		///
 		void inject_math();
 
 		///
-		///
+		/// Injects video and audio support into lua.
 		///
 		void inject_media();
 
 		///
-		///
+		/// Injects galaxy resource caches into lua.
 		///
 		void inject_resources();
 
 		///
-		///
+		/// Injects galaxy scene management into lua.
 		///
 		void inject_scenes();
 
@@ -112,7 +112,7 @@ namespace galaxy
 		///
 
 		///
-		/// Regsiter static engine services into lua.
+		/// Regsiter static engine services in lua.
 		///
 		void inject_services();
 
