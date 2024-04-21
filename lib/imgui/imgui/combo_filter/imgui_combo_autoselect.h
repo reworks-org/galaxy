a14d1ce724695856d1868e6bdf32ebad41bfca11
reworks <dev.dom.re@gmail.com>
2024-04-21 14:34:44 +0800

    imgui combo select

---
+++ lib/imgui/imgui/combo_filter/imgui_combo_autoselect.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "../imgui.h"
+#include "../imgui_internal.h"
+
+namespace ImGui
+{
+	struct ComboAutoSelectData
+	{
+		std::vector<std::string> items;
+		int                      index      = -1;
+		char                     input[128] = {};
+
+		ComboAutoSelectData(std::vector<std::string>&& hints, int selected_index = -1)
+			: items(hints)
+		{
+			if (selected_index > -1 && selected_index < (int)items.size())
+			{
+				strncpy(input, items[selected_index].c_str(), sizeof(input) - 1);
+				index = selected_index;
+			}
+		}
+	};
+
+	bool ComboAutoSelect(const char* label, ComboAutoSelectData& data, ImGuiComboFlags flags = 0);
+} // namespace ImGui
