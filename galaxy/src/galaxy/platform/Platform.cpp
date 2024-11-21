e610e9e44a4619493c3ff08d7d32edec38535dcc
reworks <dev.dom.re@gmail.com>
2024-11-21 15:15:28 +0800

    add raylib platform funcs

--- galaxy/src/galaxy/platform/Platform.cpp
+++ galaxy/src/galaxy/platform/Platform.cpp
@@ -5,4 +5,22 @@
 /// Refer to LICENSE.txt for more details.
 ///
 
+#include <raylib.h>
+
 #include "Platform.hpp"
+
+namespace galaxy
+{
+	namespace platform
+	{
+		void take_screenshot(const std::string& file)
+		{
+			TakeScreenshot(file.c_str());
+		}
+
+		void open_url(const std::string& url)
+		{
+			OpenURL(url.c_str());
+		}
+	} // namespace platform
+} // namespace galaxy
--- galaxy/src/galaxy/platform/Platform.hpp
+++ galaxy/src/galaxy/platform/Platform.hpp
@@ -16,4 +16,24 @@
 #include "galaxy/platform/specific/Unix.hpp"
 #endif
 
+namespace galaxy
+{
+	namespace platform
+	{
+		///
+		/// Takes a screenshot of current screen.
+		///
+		/// \param file File to save to. Extension defines format.
+		///
+		void take_screenshot(const std::string& file);
+
+		///
+		/// Open URL with default system browser (if available)
+		///
+		/// \param url Url to open in browser.
+		///
+		void open_url(const std::string& url);
+	} // namespace platform
+} // namespace galaxy
+
 #endif
