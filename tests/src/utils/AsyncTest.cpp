b611d1822c2234771ead2312db20e681233ed1ca
reworks <dev.dom.re@gmail.com>
2025-08-13 18:41:37 +0800

    async tests

---
+++ tests/src/utils/AsyncTest.cpp
@@ -0,0 +1,25 @@
+///
+/// StringUtilsTest.cpp
+/// tests
+///
+/// Refer to LICENSE.txt for more details.
+///
+
+#include <gtest/gtest.h>
+
+#include <galaxy/utils/Async.hpp>
+
+TEST(Utils, AsyncIsWorkDone)
+{
+	std::future<int> future = std::async(std::launch::async, [] {
+		std::this_thread::sleep_for(1s);
+		return 3;
+	});
+
+	do
+	{
+		std::this_thread::sleep_for(100ms);
+	} while (!galaxy::async::is_work_done(future));
+
+	ASSERT_EQ(future.get(), 3);
+}
