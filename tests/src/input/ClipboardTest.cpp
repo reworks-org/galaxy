c179bef0bd0c3c682bead4acc7ce5452dc1e716f
reworks <dev.dom.re@gmail.com>
2025-09-18 13:47:48 +0800

    fix clipboard test

--- tests/src/input/ClipboardTest.cpp
+++ tests/src/input/ClipboardTest.cpp
@@ -5,23 +5,21 @@
 /// Refer to LICENSE.txt for more details.
 ///
 
+#include <SDL3/SDL.h>
+
 #include <gtest/gtest.h>
 
 #include <galaxy/input/Clipboard.hpp>
 
 TEST(Clipboard, SetGet)
 {
-	auto input = "test";
+	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
+	std::string input = "test";
 
 	galaxy::input::set_clipboard(input);
-	auto get = galaxy::input::get_clipboard();
+	std::string get = galaxy::input::get_clipboard();
 
 	ASSERT_TRUE(get == input);
-}
 
-TEST(Clipboard, EmptyGet)
-{
-	auto get = galaxy::input::get_clipboard();
-
-	ASSERT_TRUE(get.empty());
+	SDL_Quit();
 }
